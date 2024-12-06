60fe5cae51dc7c64afb20074cf6b7bea286b8dd4
harish <harishinukula@gmail.com>
2024-12-06 14:06:02 +0530

    commiting the code and example

---
+++ p1.py
@@ -0,0 +1,11 @@
+# Simple Python program to add two numbers
+
+# Input: Getting numbers from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Processing: Adding the two numbers
+sum_result = num1 + num2
+
+# Output: Displaying the result
+print(f"The sum of {num1} and {num2} is {sum_result}.")
---
+++ p2.py
@@ -0,0 +1,10 @@
+# Simple Python program to check if a number is even or odd
+
+# Input: Getting a number from the user
+num = int(input("Enter a number: "))
+
+# Processing: Checking if the number is even or odd
+if num % 2 == 0:
+    print(f"{num} is an even number.")
+else:
+    print(f"{num} is an odd number.")
